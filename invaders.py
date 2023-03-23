fa1fccc0f47dc6caf0b37602abe50b91a9baf8c5
Thomas Hagelien <thomas.f.hagelien@sintef.no>
2023-03-23 10:17:03 +0100

    Moved all assets to assets/

--- invaders.py
+++ invaders.py
@@ -7,7 +7,6 @@ pygame.init()
 pygame.mixer.init()
 
 
-
 # Initialize the Pygame module, define constants, and load the sprites
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
@@ -31,6 +30,7 @@ ALIEN_SPACING = int(ALIEN_SPACING * scale_factor)
 
 alien_width = int(SCREEN_WIDTH * 0.05)
 
+
 def resize_image(image, new_width):
     aspect_ratio = image.get_height() / image.get_width()
     new_height = int(new_width * aspect_ratio)
@@ -40,30 +40,46 @@ def resize_image(image, new_width):
 def load_image(filename):
     return pygame.image.load(filename)
 
-welcome_bg = pygame.image.load("welcome_bg.png")
+
+welcome_bg = pygame.image.load("assets/welcome_bg.png")
 welcome_bg = pygame.transform.scale(welcome_bg, (SCREEN_WIDTH, SCREEN_HEIGHT))
 
-player_img = pygame.image.load("player.png")
-bullet_img = pygame.image.load("bullet.png")
-game_bg = pygame.transform.scale(load_image("game_bg.png"), (SCREEN_WIDTH, SCREEN_HEIGHT))
-alien1_img = resize_image(load_image("alien1.png"), alien_width)
-alien2_img = resize_image(load_image("alien2.png"), alien_width)
-alien3_img = resize_image(load_image("alien3.png"), alien_width)
-alien4_img = resize_image(load_image("alien4.png"), alien_width)
+player_img = pygame.image.load("assets/player.png")
+bullet_img = pygame.image.load("assets/bullet.png")
+game_bg = pygame.transform.scale(
+    load_image("assets/game_bg.png"), (SCREEN_WIDTH, SCREEN_HEIGHT)
+)
+alien1_img = resize_image(load_image("assets/alien1.png"), alien_width)
+alien2_img = resize_image(load_image("assets/alien2.png"), alien_width)
+alien3_img = resize_image(load_image("assets/alien3.png"), alien_width)
+alien4_img = resize_image(load_image("assets/alien4.png"), alien_width)
 
 
-player_img = pygame.transform.scale(player_img, (int(player_img.get_width() * scale_factor), int(player_img.get_height() * scale_factor)))
-bullet_img = pygame.transform.scale(bullet_img, (int(bullet_img.get_width() * scale_factor), int(bullet_img.get_height() * scale_factor)))
+player_img = pygame.transform.scale(
+    player_img,
+    (
+        int(player_img.get_width() * scale_factor),
+        int(player_img.get_height() * scale_factor),
+    ),
+)
+bullet_img = pygame.transform.scale(
+    bullet_img,
+    (
+        int(bullet_img.get_width() * scale_factor),
+        int(bullet_img.get_height() * scale_factor),
+    ),
+)
 
 alien_imgs = [alien1_img, alien2_img, alien2_img, alien3_img, alien4_img]
 
 # Load the sound effects
-shoot_sound = pygame.mixer.Sound("shoot.wav")
-invaderkilled_sound = pygame.mixer.Sound("invaderkilled.wav")
-explosion_sound = pygame.mixer.Sound("explosion.wav")
+shoot_sound = pygame.mixer.Sound("assets/shoot.wav")
+invaderkilled_sound = pygame.mixer.Sound("assets/invaderkilled.wav")
+explosion_sound = pygame.mixer.Sound("assets/explosion.wav")
 
 
 # Define the classes for the game objects
+
 
 class Player(pygame.sprite.Sprite):
     def __init__(self):
@@ -78,6 +94,7 @@ class Player(pygame.sprite.Sprite):
         if keys[K_RIGHT]:
             self.rect.move_ip(5, 0)
 
+
 class Bullet(pygame.sprite.Sprite):
     def __init__(self, x, y, direction):
         super().__init__()
@@ -91,6 +108,7 @@ class Bullet(pygame.sprite.Sprite):
         if self.rect.bottom < 0 or self.rect.top > SCREEN_HEIGHT:
             self.kill()
 
+
 class Alien(pygame.sprite.Sprite):
     def __init__(self, alien_type, x, y, speed):
         super().__init__()
@@ -102,17 +120,22 @@ class Alien(pygame.sprite.Sprite):
     def update(self):
         self.rect.x += self.speed
 
+
 # Define a function to display the welcome screen
 def welcome_screen(last_score, high_score):
     font = pygame.font.Font(None, 36)
 
     welcome_text = "Press 'P' to play or 'Q' to quit"
     welcome_text_surface = font.render(welcome_text, True, WHITE)
-    welcome_text_rect = welcome_text_surface.get_rect(center=(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2))
+    welcome_text_rect = welcome_text_surface.get_rect(
+        center=(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2)
+    )
 
     score_text = f"Last Score: {last_score} High Score: {high_score}"
     score_text_surface = font.render(score_text, True, WHITE)
-    score_text_rect = score_text_surface.get_rect(center=(SCREEN_WIDTH // 2, welcome_text_rect.y - 50))
+    score_text_rect = score_text_surface.get_rect(
+        center=(SCREEN_WIDTH // 2, welcome_text_rect.y - 50)
+    )
 
     while True:
         screen.blit(welcome_bg, (0, 0))
@@ -137,6 +160,7 @@ def welcome_screen(last_score, high_score):
 # Initialize the game objects, and create the main game loop:
 
 import itertools
+
 last_score = 0
 high_score = 0
 
@@ -194,7 +218,9 @@ while True:
         alien_bullet = Bullet(alien_shooter.rect.centerx, alien_shooter.rect.bottom, 1)
         bullets.add(alien_bullet)
 
-    if pygame.sprite.spritecollide(player, bullets, False) or any(alien.rect.colliderect(player.rect) for alien in aliens):
+    if pygame.sprite.spritecollide(player, bullets, False) or any(
+        alien.rect.colliderect(player.rect) for alien in aliens
+    ):
         explosion_sound.play()
         pygame.time.delay(3000)  # Wait for 3 seconds (3000 milliseconds)
         last_score = SCORE
